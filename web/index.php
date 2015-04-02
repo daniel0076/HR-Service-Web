f9f574299f1d6a4c6c4627e517f60eafdbf122d7
Daniel Tsai <idaniel.twc@gmail.com>
2015-04-02 23:25:35 +0800

    add href, fix link failure

--- web/index.php
+++ web/index.php
@@ -1,19 +1,64 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js?ver=2.0.3"></script>
     <link rel="stylesheet" type="text/css" href="static/semantic-ui/dist/semantic.css"/>
     <link rel="stylesheet" type="text/css" href="css/custom.css"/>
     <script src="static/semantic-ui/dist/semantic.js"></script>
-</head>
-<body>
-<?php include 'template/menubar.php';?>
+  </head>
+  <body>
+    <div></div>
+    <div class="ui green inverted menu">
+      <a class="item" href="index.php">
+        <i class="lightning icon"></i> Just Sudo It
+      </a>
+      <!--    <div class="ui pointing dropdown link item">
+        <div class="text">2015-DB</div>
+        <i class="dropdown icon"></i>
+        <div class="menu">
+        <div class="item">Lab1</div>
+        </div>
+        </div>-->
+        <div class="right menu">
+          <?php
+             if(!isset($_SESSION['is_authed']))
+             {
+               echo '
+              <a class="item" id="reg_button">
+                  <i class="plus icon"></i> Register
+              </a>
+              <a class="item" href="admin/login.php">
+                  <i class="sign in icon"></i> Log in
+              </a>
+              ';
+             }
+             else
+             {
+               echo '
+                <a class="item" href="admin/login.php?logout">
+                    <i class="sign out icon"></i> Log out
+                </a>
+            ';
+             }?>
+        </div>
+        <div class="ui modal">
+          <i class="close icon"></i>
+          <div class="header">Account Type</div>
+          <div class="content">
+            <div class="ui small images" align="middle">
+              <a href="admin/reg_boss.php"><img src="image/boss.png" id="boss_img"></a>
+              <div type="hidden"></div>
+              <a href="admin/reg_jobseeker.php"><img src="image/jobseeker.png" id="jobseeker_img"></a>
+            </div>
+          </div>
+        </div>
+    </div>
     <div class="ui button">
-  Follow
-</div>
-</body>
+      Follow
+    </div>
+  </body>
 </html>
--- web/template/after_login.php
+++ web/template/after_login.php
@@ -1,3 +1,3 @@
-<a class="item">
+<a class="item" href="../admin/login.php?logout">
     <i class="sign out icon"></i> Log out
 </a>
