fb9c78418923ca3f92e48cef02aea4fcf4ebfff7
Daniel Tsai <idaniel.twc@gmail.com>
2015-04-03 18:09:49 +0800

    fix link failure

--- web/template/pre_css_js.php
+++ web/template/pre_css_js.php
@@ -1,9 +1,9 @@
 <style type="text/css"></style>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script src="../static/semantic-ui/dist/semantic.min.js"></script>
-<link href="../static/semantic-ui/dist/semantic.min.css" rel="stylesheet">
-<script src="../js/custom.js"></script>
-<link href="../css/custom.css" rel="stylesheet">
+<script src="static/semantic-ui/dist/semantic.min.js"></script>
+<link href="static/semantic-ui/dist/semantic.min.css" rel="stylesheet">
+<script src="js/custom.js"></script>
+<link href="css/custom.css" rel="stylesheet">
 <script type="text/javascript">
     $(document).ready(function(){$('.ui.dropdown').dropdown({on:'hover'})});
 </script>
--- web/template/register_modal_.php
+++ web/template/register_modal_.php
@@ -3,9 +3,9 @@
     <div class="header">Account Type</div>
     <div class="content">
         <div class="ui small images" align="middle">
-            <a href="reg_boss_page.php"><img src="../image/boss.png" id="boss_img"></a>
+            <a href="boss_reg.php"><img src="image/boss.png" id="boss_img"></a>
             <div type="hidden"></div>
-            <a href="reg_jobseeker_page.php"><img src="../image/jobseeker.png" id="jobseeker_img"></a>
+            <a href="user_reg.php"><img src="image/jobseeker.png" id="jobseeker_img"></a>
         </div>
     </div>
 </div>
