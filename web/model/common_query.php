c5c7fe510fcb066dbb07cf6177a12b3110bf87fd
ActKz <brian558266@yahoo.com.tw>
2015-05-05 13:54:48 +0800

    remove redudent ifcase in commonQ

--- web/model/common_query.php
+++ web/model/common_query.php
@@ -46,36 +46,6 @@ INNER JOIN `occupation` ON recruit.occupation_id=occupation.id
     public function searchJob($occupation=null,$location=null,
         $worktime=null,$education=null,$experience=null,$salary=0,$sort="")
     {
-        if($occupation==null&&$location==null&&$worktime==null&&$education==null&&$experience==null&&$salary==0)
-        {
-            $sql="SELECT * FROM recruit_table_view";
-            if ($sort=="desc"){
-                $sort=" ORDER BY salary DESC";
-            }
-            else if($sort=="asc"){
-                $sort=" ORDER BY salary ASC";
-            }else{
-                $sort="";
-            }
-            $sql.=$sort;
-        try
-        {
-            $run=self::$myPDO->prepare($sql);
-            $run->execute();
-            $res=$run->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e)
-        {
-            echo 'Can\'t find ' . $attr . $e->getMessage();
-            return false;
-        }
-        if($res)
-        {
-            return $res;
-        }
-        return false;
-        }
-        else
-        {
         $sql="SELECT * FROM recruit_table_view WHERE
 occupation      =:occupation
 OR location      =:location
@@ -115,7 +85,6 @@ OR salary       >=:salary
             return $res;
         }
         return false;
-        }
     }
 }
 ?>
